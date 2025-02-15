94be50378d56a20f0a48c449a9d6d49f64a8cc34
Moazam05 <salmanmoazam08@gmail.com>
2025-02-15 15:52:20 +0500

    404 page

--- marketing/src/App.js
+++ marketing/src/App.js
@@ -4,6 +4,7 @@ import { StyledEngineProvider } from "@mui/material/styles";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
+import NotFound from "./components/NotFound";
 
 export default () => {
   return (
@@ -12,6 +13,8 @@ export default () => {
         <Routes>
           <Route path="/pricing" element={<Pricing />} />
           <Route path="/" element={<Landing />} />
+          {/* Add catch-all route for 404 */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </StyledEngineProvider>
