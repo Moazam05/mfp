775e54027711daa293047584b886155f919ae6f4
Moazam05 <salmanmoazam08@gmail.com>
2025-02-15 19:31:28 +0500

    build script

---
+++ marketing/config/webpack.prod.js
@@ -0,0 +1,26 @@
+const { merge } = require("webpack-merge");
+// Custom Import
+const commonConfig = require("./webpack.common");
+
+const ModuleFederationPlugin =
+  require("webpack").container.ModuleFederationPlugin;
+const packageJson = require("../package.json");
+
+const prodConfig = {
+  mode: "production",
+  output: {
+    filename: "[name].[contenthash].js",
+  },
+  plugins: [
+    new ModuleFederationPlugin({
+      name: "marketing",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./MarketingApp": "./src/bootstrap",
+      },
+      shared: packageJson.dependencies,
+    }),
+  ],
+};
+
+module.exports = merge(commonConfig, prodConfig);
