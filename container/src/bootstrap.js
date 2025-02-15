3dcf87bb3dafddefdabd70bf03a28a7516b64469
Moazam05 <salmanmoazam08@gmail.com>
2025-02-15 15:37:55 +0500

    container app

---
+++ container/src/bootstrap.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+// Get the root element using class selector
+const container = document.querySelector(".root");
+
+// Make sure the element exists
+if (!container) {
+  throw new Error("Root element not found");
+}
+
+// Create root and render
+const root = createRoot(container);
+root.render(<App />);
