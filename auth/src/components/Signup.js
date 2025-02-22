313392c227e5459732b58a6c8d2afa0bf7b6b395
Moazam05 <salmanmoazam08@gmail.com>
2025-02-22 19:37:14 +0500

    auth in a center of the screen

--- auth/src/components/Signup.js
+++ auth/src/components/Signup.js
@@ -26,175 +26,193 @@ function Copyright() {
 
 export default function SignUp({ onSignIn }) {
   return (
-    <Container component="main" maxWidth="sm">
-      <Paper
-        elevation={3}
+    <Box
+      sx={{
+        height: "calc(100vh - 70px)", // Subtracting header height
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Container
+        component="main"
+        maxWidth="sm"
         sx={{
-          mt: 8,
-          p: 4,
           display: "flex",
           flexDirection: "column",
-          alignItems: "center",
-          borderRadius: 2,
-          bgcolor: "background.paper",
-          boxShadow: "0 8px 24px rgba(0,0,0,0.12)",
+          justifyContent: "center",
+          height: "100%",
         }}
       >
-        <Avatar
+        <Paper
+          elevation={3}
           sx={{
-            width: 56,
-            height: 56,
-            bgcolor: "primary.main",
-            boxShadow: "0 4px 12px rgba(0,0,0,0.15)",
+            p: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            borderRadius: 2,
+            bgcolor: "background.paper",
+            boxShadow: "0 8px 24px rgba(0,0,0,0.12)",
           }}
         >
-          <LockOutlinedIcon sx={{ fontSize: 32 }} />
-        </Avatar>
-
-        <Typography
-          component="h1"
-          variant="h4"
-          sx={{
-            mt: 2,
-            mb: 3,
-            fontWeight: 600,
-          }}
-        >
-          Create Account
-        </Typography>
-
-        <Box
-          component="form"
-          onSubmit={(e) => e.preventDefault()}
-          sx={{ width: "100%" }}
-        >
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="fname"
-                name="firstName"
-                required
-                fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-                variant="outlined"
-                sx={{
-                  "& .MuiOutlinedInput-root": {
-                    borderRadius: 1,
-                  },
-                }}
-              />
-            </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
-                variant="outlined"
-                sx={{
-                  "& .MuiOutlinedInput-root": {
-                    borderRadius: 1,
-                  },
-                }}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                variant="outlined"
-                sx={{
-                  "& .MuiOutlinedInput-root": {
-                    borderRadius: 1,
-                  },
-                }}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-                variant="outlined"
-                sx={{
-                  "& .MuiOutlinedInput-root": {
-                    borderRadius: 1,
-                  },
-                }}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    value="allowExtraEmails"
-                    color="primary"
-                    sx={{ borderRadius: 1 }}
-                  />
-                }
-                label={
-                  <Typography variant="body2" color="text.secondary">
-                    I want to receive inspiration, marketing promotions and
-                    updates via email.
-                  </Typography>
-                }
-              />
-            </Grid>
-          </Grid>
-
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            onClick={onSignIn}
+          <Avatar
             sx={{
-              mt: 3,
-              mb: 2,
-              py: 1.5,
-              borderRadius: 1,
-              fontSize: "1.1rem",
-              textTransform: "none",
+              width: 56,
+              height: 56,
+              bgcolor: "primary.main",
               boxShadow: "0 4px 12px rgba(0,0,0,0.15)",
-              "&:hover": {
-                boxShadow: "0 6px 16px rgba(0,0,0,0.2)",
-              },
             }}
           >
-            Sign Up
-          </Button>
+            <LockOutlinedIcon sx={{ fontSize: 32 }} />
+          </Avatar>
 
-          <Grid container justifyContent="center">
-            <Grid item>
-              <Link
-                to="/auth/signin"
-                style={{
-                  textDecoration: "none",
-                  color: "primary.main",
-                  fontWeight: 500,
-                }}
-              >
-                Already have an account? Sign in
-              </Link>
+          <Typography
+            component="h1"
+            variant="h4"
+            sx={{
+              mt: 2,
+              mb: 3,
+              fontWeight: 600,
+            }}
+          >
+            Create Account
+          </Typography>
+
+          <Box
+            component="form"
+            onSubmit={(e) => e.preventDefault()}
+            sx={{ width: "100%" }}
+          >
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="fname"
+                  name="firstName"
+                  required
+                  fullWidth
+                  id="firstName"
+                  label="First Name"
+                  autoFocus
+                  variant="outlined"
+                  sx={{
+                    "& .MuiOutlinedInput-root": {
+                      borderRadius: 1,
+                    },
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  required
+                  fullWidth
+                  id="lastName"
+                  label="Last Name"
+                  name="lastName"
+                  autoComplete="lname"
+                  variant="outlined"
+                  sx={{
+                    "& .MuiOutlinedInput-root": {
+                      borderRadius: 1,
+                    },
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  variant="outlined"
+                  sx={{
+                    "& .MuiOutlinedInput-root": {
+                      borderRadius: 1,
+                    },
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                  variant="outlined"
+                  sx={{
+                    "& .MuiOutlinedInput-root": {
+                      borderRadius: 1,
+                    },
+                  }}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      value="allowExtraEmails"
+                      color="primary"
+                      sx={{ borderRadius: 1 }}
+                    />
+                  }
+                  label={
+                    <Typography variant="body2" color="text.secondary">
+                      I want to receive inspiration, marketing promotions and
+                      updates via email.
+                    </Typography>
+                  }
+                />
+              </Grid>
             </Grid>
-          </Grid>
-        </Box>
-      </Paper>
 
-      <Box sx={{ mt: 4, mb: 4 }}>
-        <Copyright />
-      </Box>
-    </Container>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              onClick={onSignIn}
+              sx={{
+                mt: 3,
+                mb: 2,
+                py: 1.5,
+                borderRadius: 1,
+                fontSize: "1.1rem",
+                textTransform: "none",
+                boxShadow: "0 4px 12px rgba(0,0,0,0.15)",
+                "&:hover": {
+                  boxShadow: "0 6px 16px rgba(0,0,0,0.2)",
+                },
+              }}
+            >
+              Sign Up
+            </Button>
+
+            <Grid container justifyContent="center">
+              <Grid item>
+                <Link
+                  to="/auth/signin"
+                  style={{
+                    textDecoration: "none",
+                    color: "primary.main",
+                    fontWeight: 500,
+                  }}
+                >
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
+            </Grid>
+          </Box>
+        </Paper>
+
+        <Box sx={{ mt: 3 }}>
+          <Copyright />
+        </Box>
+      </Container>
+    </Box>
   );
 }
