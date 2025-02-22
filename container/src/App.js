970d2b77fd83c84cd8e227567d96b3d7b8f3d37c
Moazam05 <salmanmoazam08@gmail.com>
2025-02-22 20:50:40 +0500

    lazy loading

--- container/src/App.js
+++ container/src/App.js
@@ -1,19 +1,73 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import AuthApp from "./components/AuthApp";
-import MarketingApp from "./components/MarketingApp";
 import Header from "./components/header";
+
+const AuthApp = lazy(() => import("./components/AuthApp"));
+const MarketingApp = lazy(() => import("./components/MarketingApp"));
+
+// Error Boundary Component
+class ErrorBoundary extends React.Component {
+  state = { hasError: false };
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "calc(100vh - 70px)",
+            color: "#666",
+          }}
+        >
+          Something went wrong. Please refresh the page.
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+const LoadingFallback = () => (
+  <div
+    style={{
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+      height: "calc(100vh - 70px)",
+    }}
+  >
+    <div
+      style={{
+        padding: "20px",
+        borderRadius: "8px",
+        textAlign: "center",
+        color: "#666",
+      }}
+    >
+      Loading...
+    </div>
+  </div>
+);
 
 const App = () => {
   return (
     <BrowserRouter>
       <Header />
       <hr />
-      <Routes>
-        <Route path="/auth/*" element={<AuthApp />} />
-        <Route path="/*" element={<MarketingApp />} />
-      </Routes>
+      <ErrorBoundary>
+        <Suspense fallback={<LoadingFallback />}>
+          <Routes>
+            <Route path="/auth/*" element={<AuthApp />} />
+            <Route path="/*" element={<MarketingApp />} />
+          </Routes>
+        </Suspense>
+      </ErrorBoundary>
     </BrowserRouter>
   );
 };
